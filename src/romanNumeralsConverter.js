4286831a98e820726cabc0fd737277701abe6c97
Moj <maghrout@googlemail.com>
2020-01-18 22:10:40 +0000

    more tests

--- src/romanNumeralsConverter.js
+++ src/romanNumeralsConverter.js
@@ -1,9 +1,23 @@
-const RomanNumerals = [
-    {value: 5, numeral: "V"},
-    {value: 4, numeral: "IV"},
-    {value: 1, numeral: "I"}
-]
 
+let numeral = (value, numeral) => {
+    return {value: value, numeral: numeral};
+}
+
+const RomanNumerals = [
+    numeral(1000, "M"),
+    numeral(900, "CM"),
+    numeral(500, "D"),
+    numeral(400, "CD"),
+    numeral(100, "C"),
+    numeral(90, "XC"),
+    numeral(50, "L"),
+    numeral(40, "XL"),
+    numeral(10, "X"),
+    numeral(9, "IX"),
+    numeral(5, "V"),
+    numeral(4, "IV"),
+    numeral(1, "I")
+];
 
 const RomanNumeralsConverter = (number) => {
     let romanNumerals = "";
--- test/romanNumerals.test.js
+++ test/romanNumerals.test.js
@@ -20,4 +20,36 @@ describe('RomanNumeralsConverterTest', () => {
     it('Takes 5 and returns V', () => {
         expect(romanNumeralsConverter(5)).toEqual("V");
     })
+
+    it('Takes 6 and returns VI', () => {
+        expect(romanNumeralsConverter(6)).toEqual("VI");
+    })
+
+    it('Takes 7 and returns VII', () => {
+        expect(romanNumeralsConverter(7)).toEqual("VII");
+    })
+
+    it('Takes 8 and returns VIII', () => {
+        expect(romanNumeralsConverter(8)).toEqual("VIII");
+    })
+
+    it('Takes 9 and returns IX', () => {
+        expect(romanNumeralsConverter(9)).toEqual("IX");
+    })
+
+    it('Takes 10 and returns X', () => {
+        expect(romanNumeralsConverter(10)).toEqual("X");
+    })
+
+    it('Takes 1954 and returns MCMLIV', () => {
+        expect(romanNumeralsConverter(1954)).toEqual("MCMLIV");
+    })
+
+    it('Takes 1990 and returns MCMXC', () => {
+        expect(romanNumeralsConverter(1990)).toEqual("MCMXC");
+    })
+
+    it('Takes 436 and returns CDXXXVI', () => {
+        expect(romanNumeralsConverter(436)).toEqual("CDXXXVI");
+    })
 })
